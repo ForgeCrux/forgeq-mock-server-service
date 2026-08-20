d95dc60ac644467b98dceb466daf243b58137243
Himanshu-KRE <himanshu.nag@probestack.io>
2026-08-20 15:12:17 +0530

    Initialize main [skip ci]

--- src/main/java/com/probestack/forgestudio/design/model/CreateMockRequest.java
+++ src/main/java/com/probestack/forgestudio/design/model/CreateMockRequest.java
@@ -18,7 +18,7 @@ import jakarta.annotation.Generated;
 /**
  * CreateMockRequest
  */
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-23T10:25:40.019199831Z[GMT]")public class CreateMockRequest {
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-23T11:06:32.467428401Z[GMT]")public class CreateMockRequest {
 
   private String name;
 
